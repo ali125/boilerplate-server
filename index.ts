b0a66350ce61381562f0b20b391f63f87b11ee40
Ali Moratzavi <ali.mortazavi121@gmail.com>
2024-03-30 15:24:01 +0330

    feat: added credential and cors middlewares

--- index.ts
+++ index.ts
@@ -2,17 +2,27 @@ import "reflect-metadata";
 require("dotenv").config();
 import express from "express";
 const app = express();
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { AppDataSource } from "./config/db";
 import RootRouter from "./routes";
 import { logger } from "./middleware/logEvents";
 import errorHandler from "./middleware/errorHandler";
+import credentials from "./middleware/credentials";
+import corsOptions from "./config/corsOptions";
 
 // Server port
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
 app.use(logger);
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
 
 // built-in middleware to handleurlencoded form data
 app.use(express.urlencoded({ extended: false }));
